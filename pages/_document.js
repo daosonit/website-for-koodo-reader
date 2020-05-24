405763b4325effd7cfc0025e0bfb5f9e352e5034
troyeguo <guo362429@163.com>
2020-05-24 09:45:59 +0800

    test

--- pages/_document.js
+++ pages/_document.js
@@ -104,6 +104,16 @@ class CustomDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-149740367-1"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'UA-149740367-1');
+          </script>
+
           <script>AOS.init();</script>
         </body>
       </Html>
