2cef59f6084f40fe0e013ab07008196f5efd1ab4
troyeguo <guo362429@163.com>
2020-05-24 17:05:31 +0800

    更换预览图

--- i18n.js
+++ i18n.js
@@ -3,6 +3,7 @@ const NextI18Next = require("next-i18next").default;
 module.exports = new NextI18Next({
   defaultLanguage: "cn",
   otherLanguages: ["en"],
+  browserLanguageDetection: true,
   detection: {
     lookupCookie: "next-i18next",
     order: ["cookie", "querystring", "localStorage", "path", "subdomain"],
