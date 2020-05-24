859f5b6a98a1e3a7595ee51e1049566dbdbdfe82
troyeguo <guo362429@163.com>
2020-05-24 17:58:33 +0800

    更换预览图

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  distDir: "_next",
   publicRuntimeConfig: {
-    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-      ? process.env.LOCALE_SUBPATHS
-      : 'none',
+    localeSubpaths:
+      typeof process.env.LOCALE_SUBPATHS === "string"
+        ? process.env.LOCALE_SUBPATHS
+        : "none",
   },
-}
+};
